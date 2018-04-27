fba8f4e1bca5fdb9d635f9308763fa72eaf9886a
基本上无害 <yayuneko@163.com>
2018-04-27 16:49:46 +0800

    重写物品删除 保证数据一致性 顺手给数据库加上了注释

--- application/admin/controller/GoodsType.php
+++ application/admin/controller/GoodsType.php
@@ -122,4 +122,87 @@ class GoodsType extends Base
 			}
 		}
 	}
+
+	/**
+     * 设置一条或者多条数据的状态
+     * @param $strict 严格模式要求处理的纪录的uid等于当前登陆用户UID
+     */
+    public function setStatus($model = '', $strict = false){
+        if ($model =='') {
+            $model = request()->controller();
+        }
+        $ids    = array_unique((array) input('ids/a', 0));
+        $status = input('status');
+        $setfield = input('setfield','status');
+        if (empty($ids)) {
+            $this->error('请选择要操作的数据');
+        }
+        // 获取主键
+        $status_model      = model($model);
+        $model_primary_key = $status_model->getPk();
+
+        // 获取id
+        $ids                     = is_array($ids) ? implode(',', $ids) : $ids;
+        if (empty($ids)) {
+            $this->error('请选择要操作的数据');
+        }
+        $map[$model_primary_key] = array('in', $ids);
+        // 严格模式
+        if ($strict) {
+            $map['id'] = array('eq', is_login());
+        }
+        switch ($status) {
+            case 'delete': 
+                // 删除记录
+                // 查询当前删除的项目是否有子代
+                if (in_array('pid', $status_model->getTableFields())) {
+                    $son_count = $status_model->where(array('pid' => array('in', $ids)))->count();
+                    if ($son_count > 0) {
+                        $this->error('无法删除，存在子项目！');
+                    }
+                }
+
+                // 查询当前删除模型 是否有所属属性
+                $attr_count = model('GoodsAttribute')
+                	->where(array('type_id' => array('in', $ids)))
+                	->count()
+                ;
+                if ($attr_count > 0) {
+                	$this->error('无法删除，存在子属性！请先删除所属属性');
+                }
+
+                // 查询当前删除模型 是否有所属规格
+                $spec_count = model('Spec')
+                	->where(array('type_id' => array('in', $ids)))
+                	->count()
+                ;
+                if ($spec_count > 0) {
+                	$this->error('无法删除，存在子规格！请先删除所属规格');
+                }
+
+                // 查询当前删除商品 是否存在所属商品
+                $goods_count = model('Goods')
+                	->where(array('type_id' => array('in', $ids)))
+                	->count()
+                ;
+                if ($goods_count > 0) {
+                	$this->error('无法删除，存在属于该模型的商品！请先删除所属商品');
+                }
+
+                // 删除数据
+                $result = $status_model->where($map)->delete();
+
+                if ($result) {
+                	$status_model->commit();
+                    $this->success('删除成功，不可恢复！');
+                } else {
+                	$status_model->rollback();
+                    $this->error('删除失败');
+                }
+                break;
+            default:
+                $this->error('参数错误');
+                break;
+        }
+    }
 }
